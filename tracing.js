97ad336d6c90dd2cf13c4b0bcf414182e8cef4de
ed <edlang114@gmail.com>
2024-04-23 03:13:06 -0400

    init

--- tracing.js
+++ tracing.js
@@ -4,19 +4,17 @@ const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-base');
 const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
 const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
 const { trace } = require("@opentelemetry/api");
+const { JaegerExporter } = require("@opentelemetry/exporter-jaeger");
 //Instrumentations
 const { ExpressInstrumentation } = require("opentelemetry-instrumentation-express");
 const { MongoDBInstrumentation } = require("@opentelemetry/instrumentation-mongodb");
 const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
 const { registerInstrumentations } = require("@opentelemetry/instrumentation");
-// TRACER
-const {JaegerExporter} = require('@opentelemetry/exporter-jaeger');
-const {TracerProvider} = require('@opentelemetry/sdk-trace-base');
-
 //Exporter
 module.exports = (serviceName) => {
     const exporter = new JaegerExporter({
-        serviceName: serviceName,
+        endpoint:
+        'http://localhost:14268/api/traces'
     });
 
     const provider = new NodeTracerProvider({
